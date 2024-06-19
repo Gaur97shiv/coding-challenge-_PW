36d321e597f5ced01f4f8317fa2903498844ce6b
munendra <munendrashiv97@gmail.com>
2024-06-19 19:07:05 +0530

    corrected the mistake

--- objectToArray.js
+++ objectToArray.js
@@ -1,6 +1,6 @@
 const a={1:"a",2:"b",3:"c"};
 const b=Object.keys(a);
-const len=Objects.keys(a).length;
+const len=Object.keys(a).length;
 for(let i=0;i<len;i++){
     if(b[i]==2){
         console.log(a[b[i]]);// now guess the output?
