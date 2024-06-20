e247d5032c6517c334f315fc1668c3a5f79e7e32
munendra <munendrashiv97@gmail.com>
2024-06-20 14:49:49 +0530

    explain about the closure

---
+++ Closure.js
@@ -0,0 +1,10 @@
+function x(){
+    let a=7;
+    function y(){
+        console.log(a);
+    }
+    return y;
+}
+ var z=x(); //here x will assign function y and referance of variable a +?which is known as closure
+ //console.log(z); //[Function: y]
+ z();
