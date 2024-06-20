6ae584081a9b4e2d94644ec205433a959c03f831
munendra <munendrashiv97@gmail.com>
2024-06-20 13:57:43 +0530

    added explanation of different hoisting in let and var

---
+++ DifferentHoisting.js
@@ -0,0 +1,21 @@
+/*console.log(a);
+const a=10; 
+
+output:
+
+console.log(a);
+            ^
+ReferenceError: Cannot access 'a' before initialization
+
+
+output: because in case of let a attached to script
+*/
+
+/* 
+
+console.log(a);
+var a=10;
+output: undefined 
+reason:because in case of var a attached to global variable 
+
+*/
