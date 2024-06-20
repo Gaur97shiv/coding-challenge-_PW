af036db10dc831e6f181e90726bd5de79a8eed77
munendra <munendrashiv97@gmail.com>
2024-06-20 14:10:05 +0530

    easy explanation about temporal deadzone

---
+++ TemporalDeadZone.js
@@ -0,0 +1,6 @@
+console.log(a);//varible hoisted 
+
+
+let a=10; //varible declaration 
+//temporal deadzone is a time since when the variable hoisted and the varible initilized 
+//when you try to access any  varible inside the temporal deadzone you find a error
