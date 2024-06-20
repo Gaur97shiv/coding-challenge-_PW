c9ebfe680e01af5c23b5b41b4a0cd3061814aa35
munendra <munendrashiv97@gmail.com>
2024-06-20 15:43:09 +0530

    added more info

--- Closure.js
+++ Closure.js
@@ -8,3 +8,6 @@ function x(){
  var z=x(); //here x will assign function y and referance of variable a +?which is known as closure
  //console.log(z); //[Function: y]
  z();
+
+
+ // function when returned like aboe they remember the lexical scope where it came from
