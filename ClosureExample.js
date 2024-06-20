5185601d23281028a0380ba1a9cff5f9c2f19594
munendra <munendrashiv97@gmail.com>
2024-06-20 17:30:14 +0530

    added more info

--- ClosureExample.js
+++ ClosureExample.js
@@ -7,3 +7,8 @@ function x(){
     console.log("helo");
 }
 x();
+
+//Timer and the Event Loop: JavaScript is single-threaded, meaning it can only do one thing at a time. 
+//However, it can manage asynchronous tasks using the event loop. When setTimeout is called, 
+//the browser sets up a timer in the Web APIs environment (part of the browser's runtime environment, outside of the JavaScript engine). 
+//This timer counts down the specified delay. 
